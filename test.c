902f9cd64133e795bdb4ad089c2a9b30c4176532
gustavruas <110361106+gustavruas@users.noreply.github.com>
2025-10-14 17:35:18 -0300

    Add files via upload

---
+++ test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+typedef struct Aluno{
+  int Matricula;
+  char Nome[50]; // Definindo como string até 50 chars,
+  float nota1;
+  float nota2;
+};
+
+int media(){
+  struct Aluno a1;
+
+  printf("Digite seu nome:");
+  scanf("%s", a1.Nome);
+  printf("Digite sua matricula:");
+  scanf("%d", &a1.Matricula);
+  printf("Digite sua nota1:");
+  scanf("%f", &a1.nota1);
+  printf("Digite sua nota2:");
+  scanf("%f", &a1.nota2);
+
+  double res = (a1.nota1+a1.nota2)/2;
+
+  if(a1.nota1 > 10 || a1.nota2 > 10){
+    printf("Mentiroso...");
+    return 1;
+  }
+
+  if(res >= 6){
+    printf("Aprovado!");
+  }
+  else {
+    printf("Reprovado!");
+  }
+  return 0;
+}
+
+int main(){
+  media();  
+  return 0;
+}
